e91740857ac66cdf4ec33d6496e833ef86bb7b48
Logie Mendiola <logiamendiola@iskolarngbayan.pup.edu.ph>
2023-05-22 23:24:01 +0800

    [P-7] Created Pseudocode for program flow

---
+++ calculator_with_oop.py
@@ -0,0 +1,13 @@
+#Pseudocode
+
+#Ask the user for their 1st digit
+
+#Ask the user for their 2nd digit
+
+#Performing Addition Calculation
+
+#Performing Subtraction Calculation
+
+#Performing Multiplication Calculation
+
+#Performing Division Calculation
