4e5634f01079290836710063348f57cbfe59f561
Logie Mendiola <logiamendiola@iskolarngbayan.pup.edu.ph>
2023-05-22 23:55:43 +0800

    [P-7] Added functions for user inputs in class UI

--- user_interface_class.py
+++ user_interface_class.py
@@ -2,3 +2,11 @@
 
 #Created class for User Interface:
 class UserInterface:
+    #created function to ask user for number inputs
+    def user_input_one(self):
+        digit_one = float(input("Please enter your first digit: "))
+        return digit_one
+    
+    def user_input_two(self):
+        digit_two = float(input("Please enter your second digit: "))
+        return digit_two
