15f91bde166c56c1bc1eb96f38032dbda808bb68
Logie Mendiola <logiamendiola@iskolarngbayan.pup.edu.ph>
2023-05-22 23:25:30 +0800

    [P-7] Created <class> Calculator for Cal formulas

--- calculator_class.py
+++ calculator_class.py
@@ -1,1 +1,8 @@
 #Creating two <class> for "user interface" and "computations"
+
+#Created class for Calculator formulations
+class Calculator:
+    #creating function for addition calculation
+    def addition_operation(self,digit_one,digit_two):
+        sum_calculation = digit_one + digit_two
+        return sum_calculation
