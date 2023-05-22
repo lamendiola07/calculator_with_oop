992a4a676f72b55ab90f533eb85fa584ffbdbe8c
Logie Mendiola <logiamendiola@iskolarngbayan.pup.edu.ph>
2023-05-22 23:40:31 +0800

    [P-7] Added operations to class Calculator

--- calculator_class.py
+++ calculator_class.py
@@ -6,3 +6,15 @@ class Calculator:
     def addition_operation(self,digit_one,digit_two):
         sum_calculation = digit_one + digit_two
         return sum_calculation
+    
+    def subtraction_operation(self,digit_one,digit_two):
+        subtraction_calculation = digit_one - digit_two
+        return subtraction_calculation
+    
+    def multiplication_operation(self,digit_one,digit_two):
+        multiplication_calculation = digit_one * digit_two
+        return multiplication_calculation
+    
+    def division_operation(self,digit_one,digit_two):
+        division_calculation = digit_one / digit_two
+        return division_calculation
